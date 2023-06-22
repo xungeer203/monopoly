82541f6a08eb37aadf03ec6ecb300f58f8901d4c
xungeer203 <linjun2013@gmail.com>
2023-06-22 22:04:47 +0800

    try to degug the postion of tokens

--- tokens.py
+++ tokens.py
@@ -3,15 +3,21 @@
 
 import os
 import pygame
+import constants
 
-SPAN = 1080 / 12.2  # TODO: "SPAN" to be imported from "main.py"
+# SPAN = 1080 / 12.2  
+SPAN = constants.SPAN
+WIDTH = constants.WIDTH
+HEIGHT = constants.HEIGHT
+OFFSET_X = 0
+OFFSET_Y = 0
 
 class Token(pygame.sprite.Sprite):
     def __init__(self, name):
         super().__init__()
         self.name = name
 
-        self.steps = 20  # "steps" will increase along with dice points; initial steps is 20, because the start point is right bottom of the board.
+        self.steps = 0  # "steps" will increase along with dice points; initial steps is 20, because the start point is right bottom of the board.
 
         # set image size and location
         # self.image_loc = r"pic\token_" + name + r".png"
@@ -20,33 +26,36 @@ class Token(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (79, 79))
         # self.image = self.image.convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.centerx = 980    # TODO: 500 should be imported from "main.py"
-        self.rect.centery = 1000
+        self.rect.centerx = 980/1080 * WIDTH
+        self.rect.centery = 1000/1080 * HEIGHT
 
         
         self.money = 0
         self.position = (0, 0)
 
     def location(self, dice_point):
-        print(self.steps)
+        print("before rolling dice, step: {}".format(self.steps))
         self.steps += dice_point
-        print(self.steps)
+        print("after rolling dice, step: {}".format(self.steps))
         steps_dummpy = self.steps % 40  # 40 steps in total in single loop
-        print(steps_dummpy)
+        print("steps_dummy:{}".format(steps_dummpy))
+
+        # print("before rolling dice, centerx: {}".format(self.rect.centerx))
+        # print("before rolling dice, centery: {}".format(self.rect.centery))
 
         # TODO: to be continued next time: the first rolling dice is incorrect.
         if steps_dummpy in range(0,10):
-            self.rect.centerx = steps_dummpy * SPAN
-            self.rect.centery = 0 * SPAN
+            self.rect.centerx = steps_dummpy
+            self.rect.centery = 0
         elif steps_dummpy in range(10, 20):
-            self.rect.centerx = 10 * SPAN
-            self.rect.centery = (steps_dummpy - 10) * SPAN
+            self.rect.centerx = 10
+            self.rect.centery = (steps_dummpy - 10)
         elif steps_dummpy in range(20, 30):
-            self.rect.centerx = (30 - steps_dummpy) * SPAN
-            self.rect.centery = 10 * SPAN
+            self.rect.centerx = (30 - steps_dummpy)
+            self.rect.centery = 10
         elif steps_dummpy in range(30, 40):
-            self.rect.centerx = 0 * SPAN
-            self.rect.centery = (40 - steps_dummpy) * SPAN
+            self.rect.centerx = 0
+            self.rect.centery = (40 - steps_dummpy)
         else:
             self.rect.centerx = 5  # if error, place the token in the middle of board.
             self.rect.centery = 5
